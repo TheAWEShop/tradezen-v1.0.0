e90e9004aedbcc7e0dba9b520a15d39dd9d950e9
TheAWEShop <awepowermachinery@gmail.com>
2024-08-02 10:17:22 +0000

    made auth

--- components/StockSidebar.tsx
+++ components/StockSidebar.tsx
@@ -35,6 +35,22 @@ const StockSidebar: React.FC<SidebarProps> = ({ stocks, selectedStock, onSelectS
       </SidebarBody>
     </Sidebar>
 
+    {/* <div className="">
+      <h2 className="text-2xl font-bold p-4">TradeZen</h2>
+      <ul>
+        {stocks.map((stock) => (
+          <li
+            key={stock}
+            className={`rounded-lg mx-2 py-1 pl-2 cursor-pointer ${selectedStock === stock ? 'bg-[#2f2f3b]' : ''}`}
+            onClick={() => onSelectStock(stock)}
+          >
+            {stock}
+          </li>
+        ))}
+      </ul>
+    </div> */}
+
+
   </>
 
   );
